d37a9f84c1f223aa279158a968b44a97cbb11003
hftamayo <hftamayo@gmail.com>
2023-03-31 12:13:17 -0600

    working on scaffold part 2 and showBottom

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -46,19 +46,46 @@ class _DashBoardState extends State<DashBoard> {
   final TextEditingController _titleController = TextEditingController();
   final TextEditingController _descController = TextEditingController();
 
+  void showBottomSheet(int? id) async {
+    if (id != null) {
+      final existingData =
+          _allData.firstWhere((element) => element['id'] == id);
+      _titleController.text = existingData['title'];
+      _descController.text = existingData['desc'];
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: const Color(0xFFECEAF4),
-        appBar: AppBar(
-          title: const Text("CRUD Operations"),
-        ),
-        body: _isLoading
-            ? const Center(
-                child: CircularProgressIndicator(),
-              )
-            : ListView.builder(
-                itemBuilder: (context, index) => const Card(),
-              ));
+      backgroundColor: const Color(0xFFECEAF4),
+      appBar: AppBar(
+        title: const Text("CRUD Operations"),
+      ),
+      body: _isLoading
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListView.builder(
+              itemCount: _allData.length,
+              itemBuilder: (context, index) => Card(
+                margin: const EdgeInsets.all(15),
+                child: ListTile(
+                  title: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 5),
+                    child: Text(
+                      _allData[index]['title'],
+                      style: const TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (() => showBottomSheet(context: context, builder: builder)),
+      ),
+    );
   }
 }
